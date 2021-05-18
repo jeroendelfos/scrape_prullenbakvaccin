5b9320acc989a3981a945903dfd1d3b2df37588c
Jeroen Delfos <jeroendelfos@hotmail.com>
2021-05-18 22:50:48 +0200

    add error message when postal code not parsed

--- execute_scrape.bat
+++ execute_scrape.bat
@@ -1,6 +1,15 @@
+@echo off
+
 REM put this file in the same directory as scrape_prullenbakvaccin.py
+
 set arg_postalcode=%1
+if [%1]==[] goto loop
+@echo Error: You did not pass a postal code. Use 'execute_scrape.bat "1234AX"'
+goto EOF
+
 :loop
 python scrape_prullenbakvaccin.py %arg_postalcode%
 timeout /t 110
 goto loop
+
+exit /B 1

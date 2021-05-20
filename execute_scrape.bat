e6dcc7602a04b0ffd9bce7ce6fae3e341f862caf
jeroendelfos <j.delfos@inspectie-jenv.nl>
2021-05-20 12:06:43 +0200

    give error when python file not in same dir

--- execute_scrape.bat
+++ execute_scrape.bat
@@ -1,16 +1,27 @@
 @echo off
 
+REM check if argument is passed
 if "%~1"=="" (
-    goto end
+	echo Error: You did not pass a postal code. Use 'execute_scrape.bat "1234AX"'
+	goto end
 ) else (
-    goto loop
+    	goto filecheck
 )
 
+REM check if file exists
+:filecheck
+if exist "scrape_prullenbakvaccin.py" (
+	goto loop
+) else (
+	echo Error: scrape_prullenbakvaccin.py not found. Place batch file in same directory as python file
+	goto end
+
+REM execute script until manual exit
 :loop
 python scrape_prullenbakvaccin.py %1%
 timeout /t 110
 goto loop
 
+REM exit program after error
 :end
-echo Error: You did not pass a postal code. Use 'execute_scrape.bat "1234AX"'
 exit /B 1
